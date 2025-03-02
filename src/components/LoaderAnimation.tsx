a10eb69394e640cfbfb4901b1b70cd2520264917
Akshath P <akshathpkk@gmail.com>
2025-03-02 22:45:45 +0530

    loader animation

---
+++ src/components/LoaderAnimation.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoaderAnimation = () => {
+  return (
+    <div className="">
+      <div className="w-7 h-7 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default LoaderAnimation;
